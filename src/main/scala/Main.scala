508bbb9173aa18e4b77f89c332eed11b14b62ebe
lumeav <Lukas.Mertke@htwg-konstanz.de>
2022-10-27 15:31:03 +0200

    test completed

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,9 @@
 @main def Othello: Unit = 
   println("Welcome to Othello!")
-  println(mesh(cellNum = 7))
+  println(mesh(cellNum = 8))
 
 
 val eol = sys.props("line.separator")
 def bar(cellwidth: Int = 3, cellNum: Int = 5) = ("+" + "-" * cellwidth) * cellNum + "+" + eol
 def cells(cellwidth: Int = 3, cellNum: Int = 5) = ("|" + " " * cellwidth) * cellNum + "|" + eol
-def mesh(cellwidth: Int = 3, cellNum: Int = 5) = (bar(cellwidth, cellNum) + cells(cellwidth, cellNum)) * cellNum + bar(cellwidth, cellNum)
+def mesh(cellwidth: Int = 3, cellNum: Int = 1) = (bar(cellwidth, cellNum) + cells(cellwidth, cellNum)) * cellNum + bar(cellwidth, cellNum)
--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -3,11 +3,27 @@
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers._
 
-class OthelloSpec extends AnyWordSpec:
+class OthelloSpec extends AnyWordSpec {
   "Othello" should {
     "have a bar as String of form '+---+---+---+---+---+'" in {
       bar() should be("+---+---+---+---+---+" + eol)
     }
+    "have a cells as String of form '|   |   |   |   |   »'" in {
+      cells() should be ("|   |   |   |   |   |" + eol)
+    }
+    "have mesh as String of form '+---+\n|   |\n+---+'" in {
+      mesh() should be("+---+" + eol + "|   |" + eol + "+---+" + eol)
+    }
+    "have scalable bar" in {
+      bar(1,1) should be("+-+" + eol)
+      bar(2,1) should be("+--+" + eol)
+      bar(1,2) should be("+-+-+" + eol)
+    }
+    "have scalabe cells" in {
+      cells(1,1) should be("| |" + eol)
+      cells(2,1) should be("|  |" + eol)
+      cells(1,2) should be("| | |" + eol)
+    }
   }  
-
+}
 
