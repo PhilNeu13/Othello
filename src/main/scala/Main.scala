2fd77ddab7d669eb265b852dd04b875147827d67
Philneu13 <Philneu13>
2022-11-03 14:43:56 +0100

    added basic methods

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,5 @@
+import scala.io.StdIn._
+
 @main def Othello: Unit =
   println("Welcome to Othello!")
   println(mesh(cellNum = 1))
@@ -12,3 +14,37 @@ def mesh(cellwidth: Int = 3, cellNum: Int = 1) =
     cellwidth,
     cellNum
   )
+def startGame(): Array[String] = {
+  println("Name Player 1: ")
+  val name1 = readLine()
+  print("Name Player 2: ")
+  val name2 = readLine()
+  if (names(0) == null || names(1) == null) {
+    throw new Exception("2 Players are required!")
+  }
+  val names = Array(name1, name2)
+  names
+}
+def askForMove(player: Int, names: Array[String]): Array[Int] = {
+  println(names(player - 1) + "ist an der Reihe")
+  println("x eingeben:")
+  val x = readInt()
+  println("y eingeben:")
+  val y = readInt()
+  val move = Array(x, y)
+  move
+}
+def isPossible(arr: Array[Array[Int]], x: Int, y: Int, player: Int): Boolean = {
+  if (x <= arr.length || y <= arr(0).length) {
+    true
+  } else {
+    false
+  }
+}
+def makeMove(
+    arr: Array[Array[Int]],
+    x: Int,
+    y: Int,
+    player: Int
+): Array[Array[Int]] = {}
+def printNewField(arr: Array[Array[Int]], cellwidth: Int): String = {}
