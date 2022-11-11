5556202ae3d1249f1a736744501702f3596d811b
Philneu13 <Philneu13>
2022-11-11 10:14:59 +0100

    changed names

--- src/main/scala/de/htwg/se/othello/observe/Observable.scala
+++ src/main/scala/de/htwg/se/othello/observe/Observable.scala
@@ -1,8 +1,8 @@
 package de.htwg.se.othello.observe
 
 trait Observable {
-  var subscribers: Vector[Observer] = Vector()
-  def add(s: Observer) = subscribers = subscribers :+ s
-  def remove(s: Observer) = subscribers = subscribers.filterNot(o => o == s)
-  def notifyObservers = subscribers.foreach(o => o.update)
+  var viewer: Vector[Observer] = Vector()
+  def add(s: Observer) = viewer = viewer :+ s
+  def remove(s: Observer) = viewer = viewer.filterNot(o => o == s)
+  def notifyObservers = viewer.foreach(o => o.update)
 }
