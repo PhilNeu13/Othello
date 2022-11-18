a62f4aca44898d91fde92937ccfaa437d487242d
Philneu13 <Philneu13>
2022-11-18 10:36:23 +0100

    added test

--- src/main/scala/de/htwg/se/othello/model/Player.scala
+++ src/main/scala/de/htwg/se/othello/model/Player.scala
@@ -1,3 +1,5 @@
 package de.htwg.se.othello.model
 
-case class Player(name: String, stone: Stone)
+case class Player(name: String, stone: Stone) {
+  override def toString: String = s"$name " + stone.toString
+}
