b7c8f7def206a8c2d5202f10b65bcea5109fd848
Philneu13 <Philneu13>
2022-11-29 08:38:47 +0100

    improved template method

--- src/main/scala/de/htwg/se/othello/aview/UI.scala
+++ src/main/scala/de/htwg/se/othello/aview/UI.scala
@@ -6,8 +6,10 @@ import model.{Stone, MoveCoordinates, Player, Field}
 import scala.io.StdIn.readLine
 import de.htwg.se.othello.util.{Observer, PlayerState}
 
-trait UI(controller: Controller) extends Observer {
-  def start: Unit
-  def controllMove(eingabe: String): Unit
+abstract class UI(controller: Controller) extends Observer {
+  def start: Unit =
+    update
+    controllMove
+  def controllMove: Unit
   def makeAMove(eingabe: String): Option[MoveCoordinates]
 }
