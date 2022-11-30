8dd3ea238f875170b5bab5a513bd5aba3e816669
Philneu13 <Philneu13>
2022-11-30 23:43:49 +0100

    test

--- src/test/scala/de/htwg/se/othello/PlayerstateSpec.scala
+++ src/test/scala/de/htwg/se/othello/PlayerstateSpec.scala
@@ -5,4 +5,8 @@ import de.htwg.se.othello.model.{MoveCoordinates, Stone}
 import org.scalatest.matchers.should.Matchers.*
 import org.scalatest.wordspec.AnyWordSpec
 
-class PlayerstateSpec {}
+class PlayerstateSpec {
+  val pState = PlayerState
+  pState.stratPlayer1(MoveCoordinates(Stone.B, 1, 1)) should be(true)
+  pState.stratPlayer2(MoveCoordinates(Stone.W, 1, 1)) should be(true)
+}
